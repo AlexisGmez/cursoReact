c929fc0d9a1ad894d6399452ab1cbb6940990554
Alexis Medrano <alexisgmez65@gmail.com>
2022-06-20 13:14:59 -0500

    formulario no controlado

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,12 @@
-import React from 'react'
-import Frutas from './components/Frutas.jsx'
-import Contador from './components/Contador.jsx'
+import FormNoControlado from "./components/FormNoControlado"
 
-const App = () =>{
-    const frutas = ["🍏","🍐","🍌"];
-    const objeto ={
-        primary: "text-primary",
-        danger: "text-danger"
-    }
-    const user =false;
-    const saludo = "saludo desde variable";
-
-    const SaludoBienvenida =() =>(
-        <h2 className={objeto.danger}>Bienvenida</h2>
-    )
-    
-    const SaludoDespedida = () =>(
-        <h2 className={objeto.primary}>Despedida</h2>
-    )
-    
-    const funcionClick = () =>{
-        console.log("me diste click");
-    }
-    return(
-        <div className="container " >
-            <p>{saludo}</p>
-            {user ? <SaludoBienvenida/> : <SaludoDespedida/>}
-            <Frutas frutas = {frutas}/>
-            <Contador/>
-            
-            <button className='btn btn-primary' onClick={funcionClick}>buttton</button>
-        </div>
-        
-       
-    );
+function App() {
+  return (
+    <div className="container">
+        <h1>Formularios</h1>
+        <FormNoControlado/>
+    </div>
+  )
 }
+
 export default App
